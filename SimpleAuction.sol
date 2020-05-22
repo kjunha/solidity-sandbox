d3a85135449a3cea8848c21f1aee44861f9ad163
kjunha <kjunha77@gmail.com>
2020-05-22 14:21:00 +0900

    add comments

--- SimpleAuction.sol
+++ SimpleAuction.sol
@@ -1,4 +1,11 @@
 pragma solidity ^0.4.24;
+/*
+    An Auctionier can hold an Auction.
+    The Auctionier can select beneficiary and time limit
+    
+    A Smart Contract that auctionier can have control to start and end the aution can be considered.
+
+*/
 contract SimpleAuction {
     //Auction information
     address public beneficiary;
@@ -30,6 +37,11 @@ contract SimpleAuction {
         emit HighestBidIncreased(msg.sender, msg.value);
     }
     
+    //A transaction for message sender should only happen in one function.
+    //For example, it is not a good idea to revert money back to the previous highest bidder
+    //when new highest bid is reveived. Instead, when new highest bid is accepted, the previous bid is
+    //pended for previous bidder to revert back. (withdraw)
+
     function withdraw() public returns(bool) {
         uint amount = pendingReturns[msg.sender];
         if(amount > 0) {
@@ -43,6 +55,7 @@ contract SimpleAuction {
         return true;
     }
     
+    //Future update: all pended amount is reverted when the auction is closed.
     function auctionEnded() public {
         require(now >= auctionEnd);
         require(!ended);
